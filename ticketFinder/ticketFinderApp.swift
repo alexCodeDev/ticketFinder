ea6b266f4b1c702d487f192bc6a60017a4800ba9
alexCodeDev <alexandrmakarevskiy@gmail.com>
2024-05-28 14:59:17 +0500

    Initial Commit

---
+++ ticketFinder/ticketFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ticketFinderApp.swift
+//  ticketFinder
+//
+//  Created by alexCode on 28/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ticketFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
